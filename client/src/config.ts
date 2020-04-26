205b2456b2bc33de776c00b92d29c7467a8736c4
Anamika Maurya <Anamika.Maurya@ecs-digital.co.uk>
2020-04-27 02:13:25 +0530

    chore: Updated client config file with configuration details

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,9 +4,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map
-  //domain: Auth0 Domain
-  //clientId: Auth0 Client ID
-  domain: 'dev-rsplt9p2.auth0.com',
-  clientId: 'yxDnjrbaJQC6MXrSMdcZvloxClbAwg9z',
+  domain: 'dev-rsplt9p2.auth0.com',               //domain: Auth0 Domain
+  clientId: 'yxDnjrbaJQC6MXrSMdcZvloxClbAwg9z',   //clientId: Auth0 Client ID
   callbackUrl: 'http://localhost:3000/callback'
 }
